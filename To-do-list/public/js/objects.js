92bf451fbc41a0449ae485849ba3e54c0e4f8a58
ekolev11 <ekollev11@gmail.com>
2025-01-09 20:36:14 +0200

    fix note editing
    extract the ID of the current li element and assign it properly to the txtarea and p elements so that we can edit each "to do"

--- To-do-list/public/js/objects.js
+++ To-do-list/public/js/objects.js
@@ -82,14 +82,20 @@ document.addEventListener("DOMContentLoaded", function() {
         input.classList.add("rightItems", "rightItemsInput", "editField");
         input.style.height = paragraphStyles.height;
         input.value = paragraph.textContent;
-        paragraph.style.display = 'none';
+        paragraph.style.display = 'none'; 
         input.style.display = 'flex';   
     }
         document.querySelector(".editField").addEventListener("keydown", function (e){
         let key = e.key;
-            
+        
         if (key === "Enter"){
             e.preventDefault();
+            let elementID = clickedElement.closest("li");
+            //try to trim the output here /\ and get the ID of the element so we can assign it to the elements below
+            //so that we can be able to edit each note instead of only the first.
+            console.log(elementID);
+            
+            
             let editField = document.querySelector(".editInput");
             let pElement = document.querySelector(".text")
              pElement.textContent = editField.value;
